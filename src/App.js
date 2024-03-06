0826127417bf0e061d4bc0eeaa356b5a2e81180e
Success Bright <bs3webdevelop@gmail.com>
2024-03-06 19:04:01 +0100

    update

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ export default function Profile() {
   return (
     <>
       <h1>{user.name}</h1>
-      <p>He is {user.age} old</p>
+      <p>`he is {user.age} old</p>
       <img
         className="Avatar"
         src="{user.imageUrl}"
